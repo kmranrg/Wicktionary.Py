e8bffe739593f4c275127fea5f08ee94b23a6f9a
Kumar Anurag <kan@Kumars-MacBook-Air.local>
2022-12-03 10:12:39 +0530

    added search field and floating button

--- main.py
+++ main.py
@@ -2,27 +2,38 @@ import flet as ft
 
 def main(page: ft.Page):
     # setting the app title
-    page.title = "Wicktionary"
+    page.title="Wicktionary"
+
+    # enabling scroll in the page
+    page.scroll="hidden"
 
     # setting the app background color
-    page.bgcolor = "#FFFFFF"
+    page.theme_mode="light"
 
     # show BottomSheet function
     def show_bs(e):
-        bs.open = True
+        bs.open=True
         bs.update()
+
+    def search_click(e):
+        pass
+
+    # search
+    search_word = ft.TextField(hint_text="search anything...", color="#038F75", border_color="#038F75", expand=True)
+    search_button = ft.FloatingActionButton(icon=ft.icons.SEARCH, on_click=search_click)
 
     # BottomSheet for Developer Info
     bs = ft.BottomSheet(
         ft.Container(
             ft.Column(
                 [
-                    ft.Text("Developer: Kumar Anurag || Instagram: kmranrg"),
+                    ft.Text("Developer: Anurag || Inspired By: Bhavani", color=ft.colors.WHITE),
                 ],
                 tight=True,
                 horizontal_alignment="center"
             ),
             padding=10,
+            bgcolor="#04705C"
         ),
         open=False,
     )
@@ -41,6 +52,14 @@ def main(page: ft.Page):
         ],
     )
 
-    page.update()
+    page.add(
+        ft.Row(
+            [
+                search_word,
+                search_button
+            ],
+            alignment="center"
+        )
+    )
 
 ft.app(target=main, view=ft.WEB_BROWSER)
